8593be46ff909ce0f8c7b4066b8b53d4aa4ac2e8
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-04-06 01:52:51 +0530

    write logic for insert a new book in record

--- BRMapp/views.py
+++ BRMapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from BRMapp.forms import NewBookForm
 # Create your views here.
+
+
+def newBook(request):
+    form=NewBookForm()
+    res=render(request,'BRMapp/new_book.html',{'form':form})
+    return res
+
+def add(request):
+    if request.method=='POST':
+        form=NewBookForm(request.POST)
+        book=models.Book()
+        book.title=form.data['title']
+        book.price=form.data['price']
+        book.author=form.data['author']
+        book.publisher=form.data['publisher']
+        book.save()
+        s="Record Stored<br><a href='/BRMapp/view-books'>View all Books </a>"
+        retuurn HttpResponse(s)
