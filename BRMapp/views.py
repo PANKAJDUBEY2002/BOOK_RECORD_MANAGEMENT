f8e75eb35246953be0ef9f32136880a0812db731
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-04-07 01:59:41 +0530

    denied to acees page without login

--- BRMapp/views.py
+++ BRMapp/views.py
@@ -3,11 +3,12 @@ from BRMapp.forms import NewBookForm,SearchForm
 from BRMapp import models
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def userLogout(request):
     logout(request)
-    return HttpResponseRedirect('/BRMapp/login/')
+    return HttpResponseRedirect('/BRMapp/login')
 
 def userLogin(request):
     data={}
@@ -24,31 +25,33 @@ def userLogin(request):
             return res
     else:
         return render(request,'BRMapp/user_login.html',data)
+@login_required(login_url="/BRMapp/login")
 def search(request):
     form=SearchForm(request.POST)
     books=models.Book.objects.filter(title=form.data['title'])
     res=render(request,'BRMapp/search_book.html',{'form':form,'books':books})
     return res
 
-
+@login_required(login_url="/BRMapp/login")
 def searchBook(request):
     form=SearchForm()
     res=render(request,'BRMapp/search_book.html',{'form':form})
     return res
 
-
+@login_required(login_url="/BRMapp/login")
 def editBook(request):
     book=models.Book.objects.get(id=request.GET['bookid'])
     fields={'title':book.title,'price':book.price,'author':book.author,'publisher':book.publisher}
     form=NewBookForm(initial=fields)
     res=render(request,'BRMapp/edit_book.html',{'form':form,'book':book})
     return res
-
+@login_required(login_url="/BRMapp/login")
 def deleteBook(request):
     bookid=request.GET['bookid']
     book=models.Book.objects.filter(id=bookid)
     book.delete()
     return HttpResponseRedirect('BRMapp/view-books')
+@login_required(login_url="/BRMapp/login")
 def edit(request):
     form=NewBookForm(request.POST)
     book=models.Book()
@@ -59,16 +62,17 @@ def edit(request):
     book.pubisher=form.data['publisher']
     book.save()
     return HttpResponseRedirect('/BRMapp/view-books')
-
+@login_required(login_url="/BRMapp/login")
 def viewBooks(request):
     books=models.Book.objects.all()
     res=render(request,'BRMapp/view_book.html',{'books':books})
     return res
+@login_required(login_url="/BRMapp/login")
 def newBook(request):
     form=NewBookForm()
     res=render(request,'BRMapp/new_book.html',{'form':form})
     return res
-
+@login_required(login_url="/BRMapp/login")
 def add(request):
     if request.method=='POST':
         form=NewBookForm(request.POST)
