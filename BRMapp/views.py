284fcbdce4ec57be5fc14a0ec6ea4cd43ff13b89
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-04-06 14:30:32 +0530

    add sectiion edit particular book present in record

--- BRMapp/views.py
+++ BRMapp/views.py
@@ -3,6 +3,23 @@ from BRMapp.forms import NewBookForm
 from BRMapp import models
 from django.http import HttpResponse
 # Create your views here.
+def editBook(request):
+    book=models.Book.objects.get(id=request.get['bookid'])
+    fields={'title':book.title,'price':book.price,'author':book.author,'publisher':book.publisher}
+    form=NewBookForm(initial=fields)
+    res=render(request,'BRMapp/edit_book.html',{'form':form,'book':book})
+    return res
+
+
+def edit(request):
+    form=NewBookForm(request.post)
+    book=models.Book()
+    book.id=request.POST['bookid']
+    book.title=form.data['title']
+    book.price=form.data['price']
+    book.author=form.data['author']
+    book.pubisher=form.data['publisher']
+    book.save()
 
 def viewBooks(request):
     books=models.Book.objects.all()
